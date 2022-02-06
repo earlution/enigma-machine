e5f09a9c9ab1a26542d2d7f149bd10fc88f1e9a0
ruari <earlution@gmail.com>
2022-02-06 16:29:30 +0000

    Refactored TestRotor tests to reflect expected, actual as order of parameters.

--- tests/test_rotor.py
+++ tests/test_rotor.py
@@ -5,27 +5,64 @@ from rotor import *
 class TestRotors(unittest.TestCase):
     def test_rotor_i_a(self):
         test_rotor = Rotor('I')
-        self.assertEqual(test_rotor.rotor['A'], 'E')
+        self.assertEqual('E', test_rotor.rotor['A'])
 
     def test_rotor_i_n(self):
         test_rotor = Rotor('I')
-        self.assertEqual(test_rotor.rotor['N'], 'W')
+        self.assertEqual('W', test_rotor.rotor['N'])
 
     def test_rotor_i_z(self):
         test_rotor = Rotor('I')
-        self.assertEqual(test_rotor.rotor['Z'], 'J')
+        self.assertEqual('J', test_rotor.rotor['Z'])
 
     def test_rotor_ii_a(self):
         test_rotor = Rotor('II')
-        self.assertEqual(test_rotor.rotor['A'], 'A')
+        self.assertEqual('A', test_rotor.rotor['A'])
 
     def test_rotor_ii_n(self):
         test_rotor = Rotor('II')
-        self.assertEqual(test_rotor.rotor['N'], 'T')
+        self.assertEqual('T', test_rotor.rotor['N'])
 
     def test_rotor_ii_z(self):
         test_rotor = Rotor('II')
-        self.assertEqual(test_rotor.rotor['Z'], 'E')
+        self.assertEqual('E', test_rotor.rotor['Z'])
+
+    def test_rotor_iii_a(self):
+        test_rotor = Rotor('III')
+        self.assertEqual('B', test_rotor.rotor['A'])
+
+    def test_rotor_iii_n(self):
+        test_rotor = Rotor('III')
+        self.assertEqual('N', test_rotor.rotor['N'])
+
+    def test_rotor_iii_z(self):
+        test_rotor = Rotor('III')
+        self.assertEqual('O', test_rotor.rotor['Z'])
+
+    def test_rotor_iv_a(self):
+        test_rotor = Rotor('IV')
+        self.assertEqual('E', test_rotor.rotor['A'])
+
+    def test_rotor_iv_n(self):
+        test_rotor = Rotor('IV')
+        self.assertEqual('H', test_rotor.rotor['N'])
+
+    def test_rotor_iv_z(self):
+        test_rotor = Rotor('IV')
+        self.assertEqual('B', test_rotor.rotor['Z'])
+
+    def test_rotor_v_a(self):
+        test_rotor = Rotor('V')
+        self.assertEqual('V', test_rotor.rotor['A'])
+
+    def test_rotor_v_n(self):
+        test_rotor = Rotor('V')
+        self.assertEqual('H', test_rotor.rotor['N'])
+
+    def test_rotor_v_z(self):
+        test_rotor = Rotor('V')
+        self.assertEqual('K', test_rotor.rotor['Z'])
+        self.assertEqual()
 
 
 if __name__ == '__main__':
