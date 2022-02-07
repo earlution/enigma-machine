39f8d1578039098bd85a7782c87205f07d4d22aa
ruari <earlution@gmail.com>
2022-02-07 23:35:52 +0000

    Fixed bug in patch(), could add leads past 10

--- source/plugboard.py
+++ source/plugboard.py
@@ -5,22 +5,23 @@ class Plugboard:
         uppercase_letters_string = string.ascii_uppercase
         uppercase_letters_list = list(uppercase_letters_string)
         self.mappings = {k: v for k, v in zip(uppercase_letters_list, uppercase_letters_list)}
-        # to do imp. self.plugleads
         self.plugleads = list()
         self.__max_num_of_plugleads = 10
         self.__num_of_plugleads = 0
 
     def patch(self, source, target):
-        if len(self.plugleads) <= self.__max_num_of_plugleads:
-            self.__num_of_plugleads += 1
-            pluglead = Pluglead(source.upper(), target.upper())
-            self.plugleadsappend(pluglead)
-            self.mappings.update(pluglead.mapping1)
-            self.mappings.update(pluglead.mapping2)
-        else:
+        if len(self.plugleads) == self.__max_num_of_plugleads:
             # @TODO imp. better error handling here
             print('No available plugleads')
-            raise PermissionError  # don't have permission to add another lead
+            # raise PermissionError  # don't have permission to add another lead
+        else:
+            pluglead = Pluglead(source.upper(), target.upper())
+            self.plugleads.append(pluglead)
+            self.mappings.update(pluglead.mapping1)
+            self.mappings.update(pluglead.mapping2)
+            self.__num_of_plugleads += 1
+
+
 
     # @TODO imp. un_patch: delete from self.plugleads; reset self.mappings
     def un_patch(self, letter):
@@ -32,3 +33,19 @@ class Pluglead:
         self.mapping1 = {source: target}
         self.mapping2 = dict()
         self.mapping2 = {target: source}
+
+
+if __name__ == "__main__":
+    from plugboard import *
+    pb = Plugboard()
+    pb.patch('a', 'b')
+    pb.patch('C', 'K')
+    pb.patch('E', 'F')
+    pb.patch('G', 'H')
+    pb.patch('I', 'J')
+    pb.patch('D', 'L')
+    pb.patch('M', 'N')
+    pb.patch('O', 'P')
+    pb.patch('Q', 'R')
+    pb.patch('S', 'U')
+    pb.patch('X', 'Z')
