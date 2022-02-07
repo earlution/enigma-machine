0209e3cfcf08423161fdf689a5070be12c088611
ruari <earlution@gmail.com>
2022-02-07 22:36:32 +0000

    WiP Plugboard()...
    - if-else in patch() DOES NOT work, for some reason will increment __num_of_plugleads beyond 10, despite __num_of_plugleads <= __max_num_of_plugleads codition of the if-else...

--- source/plugboard.py
+++ source/plugboard.py
@@ -1,23 +1,30 @@
 import string
-from pluglead import PlugLead
-
 
 class Plugboard:
     def __init__(self):
         uppercase_letters_string = string.ascii_uppercase
         uppercase_letters_list = list(uppercase_letters_string)
-        self.mappings = [{k: v} for k, v in zip(uppercase_letters_list, uppercase_letters_list)]
+        self.mappings = {k: v for k, v in zip(uppercase_letters_list, uppercase_letters_list)}
+        # to do imp. self.plugleads
+        self.plugleads = dict.fromkeys(list(range(1, 11)))
         self.__max_num_of_plugleads = 10
         self.__num_of_plugleads = 0
-        self.__plugleads = [{key} for key in range(1, 11)]
 
-    def patch(self, pluglead: PlugLead):
-        if self.__num_of_plugleads <= 10:
-            self.mappings.update(pluglead)
+    def patch(self, source, target):
+        if self.__num_of_plugleads <= self.__max_num_of_plugleads:
+            self.__num_of_plugleads += 1
+            pluglead = Pluglead(source.upper(), target.upper())
+            self.plugleads[self.__num_of_plugleads] = pluglead
+            self.mappings.update(pluglead.mapping1)
+            self.mappings.update(pluglead.mapping2)
         else:
             # @TODO imp. better error handling here
             print('No available plugleads')
+            raise PermissionError  # don't have permission to add another lead
 
+    # @TODO imp. un_patch: delete from self.plugleads; reset self.mappings
+    def un_patch(self):
+        raise NotImplementedError
 
 class Pluglead:
     def __init__(self, source, target):
