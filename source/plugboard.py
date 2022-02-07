cc0572f08c3b9c21346cd1b3dbcbe0ad70536877
ruari <earlution@gmail.com>
2022-02-07 22:54:54 +0000

    Changed self.plugleads from dict() to list()

--- source/plugboard.py
+++ source/plugboard.py
@@ -6,15 +6,15 @@ class Plugboard:
         uppercase_letters_list = list(uppercase_letters_string)
         self.mappings = {k: v for k, v in zip(uppercase_letters_list, uppercase_letters_list)}
         # to do imp. self.plugleads
-        self.plugleads = dict.fromkeys(list(range(1, 11)))
+        self.plugleads = list()
         self.__max_num_of_plugleads = 10
         self.__num_of_plugleads = 0
 
     def patch(self, source, target):
-        if self.__num_of_plugleads <= self.__max_num_of_plugleads:
+        if len(self.plugleads) <= self.__max_num_of_plugleads:
             self.__num_of_plugleads += 1
             pluglead = Pluglead(source.upper(), target.upper())
-            self.plugleads[self.__num_of_plugleads] = pluglead
+            self.plugleadsappend(pluglead)
             self.mappings.update(pluglead.mapping1)
             self.mappings.update(pluglead.mapping2)
         else:
@@ -23,7 +23,7 @@ class Plugboard:
             raise PermissionError  # don't have permission to add another lead
 
     # @TODO imp. un_patch: delete from self.plugleads; reset self.mappings
-    def un_patch(self):
+    def un_patch(self, letter):
         raise NotImplementedError
 
 class Pluglead:
