6e419b99387ba5feea6159b3a5f8a80b4113d11e
ruari <earlution@gmail.com>
2022-02-07 23:40:21 +0000

    Added test for > 10 Plugleads

--- tests/test_plugboard.py
+++ tests/test_plugboard.py
@@ -10,6 +10,22 @@ class TestPlugboardBaseEncoding(unittest.TestCase):
         result = test_plugboard.mappings['A']
         self.assertEqual('B', result)
 
+class TestPlugboardPlugleads(unittest.TestCase):
+    def test_add_11th_pluglead(self):
+        test_plugboard = Plugboard()
+        test_plugboard.patch('A', 'B')
+        test_plugboard.patch('C', 'K')
+        test_plugboard.patch('E', 'F')
+        test_plugboard.patch('G', 'H')
+        test_plugboard.patch('I', 'J')
+        test_plugboard.patch('D', 'L')
+        test_plugboard.patch('M', 'N')
+        test_plugboard.patch('O', 'P')
+        test_plugboard.patch('Q', 'R')
+        test_plugboard.patch('S', 'U')
+        test_plugboard.patch('X', 'Z')
+        # @TODO do this test properly
+
 
 if __name__ == '__main__':
     unittest.main()
