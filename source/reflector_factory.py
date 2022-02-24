9e4bb391f1354b1cd3e41c1c7a12a6ad38fe71dd
earlution <earlution@gmail.com>
2022-02-24 16:37:10 +0000

    Added ConcreteReflectorFactory
    - with func. stubs for reflectors A - C

--- source/reflector_factory.py
+++ source/reflector_factory.py
@@ -12,11 +12,41 @@ class AbstractReflectorFactory(ABC):
     are called a family and are related by a high-level theme or concept. Products of one family are usually able to
     collaborate among themselves. A family of products may have several variants, but the products of one variant are
     incompatible with products of another.
-    source: https://refactoring.guru/design-patterns/abstract-factory/python/example
     """
 
     @abstractmethod
     def create_reflector(self) -> AbstractReflector:
+        pass
+
+
+class ConcreteReflectorFactory(AbstractReflectorFactory):
+    """
+
+    Concrete Factories produce a family of products that belong to a single variant. The factory guarantees that
+    resulting products are compatible. Note that signatures of the Concrete Factory's methods return an abstract
+    product, while inside the method a concrete product is instantiated.
+    """
+
+    def create_reflector_a(self):
+        """
+
+        """
+
+        pass
+
+    def create_reflector_b(self):
+        """
+
+        """
+
+        pass
+
+    def create_reflector_c(self):
+        """
+
+        """
+
+        pass
 
 
 class Reflector(ABC):
