c4d70bc52314f9b977470fd8e2c4499ac657cf5a
earlution <earlution@gmail.com>
2022-02-24 11:19:37 +0000

    Updated test_reflector.py

---
+++ tests/test_reflector.py
@@ -0,0 +1,11 @@
+import unittest
+from source.reflector import *
+
+# @TODO test various reversed rotor encodings
+class TestReflectorReversedRotorEncodings(unittest.TestCase):
+    def test_rotor_i_rev_a(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
