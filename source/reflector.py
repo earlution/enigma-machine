bdf58cc9c7a37b1f6c48e020e335902230ce8af0
earlution <earlution@gmail.com>
2022-02-24 11:45:34 +0000

    Updated Reflectors_Factory
    - added constructor - WiP
      - references var. that does not exist
    - added show_reflector()
      - prolly a temp. procedure

--- source/reflector.py
+++ source/reflector.py
@@ -1,4 +1,5 @@
-class Reflector:
+# @TODO is this the correct name?
+class Reflectors_Factory:
     """
 
     """
@@ -13,3 +14,23 @@ class Reflector:
                           'B', 'L', 'M', 'D', 'X', 'Z', 'V', 'F', 'T', 'H', 'R', 'G', 'S']
     __reflector_c_thin = ['R', 'D', 'O', 'B', 'J', 'N', 'T', 'K', 'V', 'E', 'H', 'M', 'L',
                           'F', 'C', 'W', 'Z', 'A', 'X', 'G', 'Y', 'I', 'P', 'S', 'U', 'Q']
+
+
+    def __init__(self, reflector_factory = None):
+        """reflector_factory is the abstract factory
+
+        """
+
+        self.reflector_factory = reflectors_factory
+
+
+    def show_reflector(self):
+        """ creates and shoes reflectors using the abstract factory
+
+        """
+
+        reflector = self.reflector_factory()
+
+        print(f'We have a reflector{reflector}')
+        print('it has the following encoding pattern: ')
+        print(f'{reflector.encoding}')
