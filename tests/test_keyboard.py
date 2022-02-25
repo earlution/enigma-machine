247213f5ffacf51137e55bb4ec4f1fbbc36ec064
earlution <earlution@gmail.com>
2022-02-25 13:25:37 +0000

    Added some tests for TestKeyboard

--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -1,9 +1,22 @@
 import unittest
+from keyboard import Keyboard
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestKeyboard(unittest.TestCase):
+    def test_press_sml_a(self):
+        kb = Keyboard()
+        result = kb.press('a')
+        self.assertEqual('A', result)
+
+    def test_press_big_a(self):
+        kb = Keyboard()
+        result = kb.press('A')
+        self.assertEqual('A', result)
+
+    def test_press_big_a(self):
+        kb = Keyboard()
+        result = kb.press('1')
+        self.assertEqual('A', result)
 
 
 if __name__ == '__main__':
