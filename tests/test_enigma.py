19a5184db8733d06cea6562d7f49d0fd14bb0f83
earlution <earlution@gmail.com>
2022-02-25 17:18:10 +0000

    Added test to TestEnigma
    - def test_enigma_jupyter_nb_case()
    - to test the scenario fromm the Jupyter notebook

--- tests/test_enigma.py
+++ tests/test_enigma.py
@@ -1,9 +1,19 @@
 import unittest
+from enigma import Enigma
 
 
 class TestEnigma(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_enigma_jupyter_nb_case(self):
+        enigma = Enigma()
+        enigma.rotors.setup_rotors('I', 'II', 'III')
+        enigma.rotors.rotor_3.set_position(26)
+        enigma.reflector.setup('B')
+        input = 'A'
+        post_rotors = enigma.rotors.encode(input)
+        post_reflector = enigma.reflector.encode(post_rotors)
+        post_rotors_rev = enigma.rotors.encode(post_reflector, True)
+        print(post_rotors_rev)
+
 
 
 if __name__ == '__main__':
