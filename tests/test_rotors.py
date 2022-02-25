eaea2ccf0d8a18db9f2541b450ad931d31ba5fd3
earlution <earlution@gmail.com>
2022-02-25 10:46:35 +0000

    Updated TestRotorListValue tests
    - to reflect list-based schema

--- tests/test_rotors.py
+++ tests/test_rotors.py
@@ -26,87 +26,83 @@ class TestEnigma(unittest.TestCase):
 class TestRotorListValue(unittest.TestCase):
     def test_rotor_i_a(self):
         test_rotor = Rotor('I')
-        self.assertEqual('E', test_rotor.rotor['A'])
-
-    def test_rotor_i_a_little(self):
-        test_rotor = Rotor('I')
-        self.assertEqual('E', test_rotor.rotor['a'])
+        self.assertEqual('E', test_rotor.rotor[0])
 
     def test_rotor_i_n(self):
         test_rotor = Rotor('I')
-        self.assertEqual('W', test_rotor.rotor['N'])
+        self.assertEqual('W', test_rotor.rotor[13])
 
     def test_rotor_i_z(self):
         test_rotor = Rotor('I')
-        self.assertEqual('J', test_rotor.rotor['Z'])
+        self.assertEqual('J', test_rotor.rotor[25])
 
     def test_rotor_ii_a(self):
         test_rotor = Rotor('II')
-        self.assertEqual('A', test_rotor.rotor['A'])
+        self.assertEqual('A', test_rotor.rotor[0])
 
     def test_rotor_ii_n(self):
         test_rotor = Rotor('II')
-        self.assertEqual('T', test_rotor.rotor['N'])
+        self.assertEqual('T', test_rotor.rotor[13])
 
     def test_rotor_ii_z(self):
         test_rotor = Rotor('II')
-        self.assertEqual('E', test_rotor.rotor['Z'])
+        self.assertEqual('E', test_rotor.rotor[25])
 
     def test_rotor_iii_a(self):
         test_rotor = Rotor('III')
-        self.assertEqual('B', test_rotor.rotor['A'])
+        self.assertEqual('B', test_rotor.rotor[0])
 
     def test_rotor_iii_n(self):
         test_rotor = Rotor('III')
-        self.assertEqual('N', test_rotor.rotor['N'])
+        self.assertEqual('N', test_rotor.rotor[13])
 
     def test_rotor_iii_z(self):
         test_rotor = Rotor('III')
-        self.assertEqual('O', test_rotor.rotor['Z'])
+        self.assertEqual('O', test_rotor.rotor[25])
 
     def test_rotor_iv_a(self):
         test_rotor = Rotor('IV')
-        self.assertEqual('E', test_rotor.rotor['A'])
+        self.assertEqual('E', test_rotor.rotor[0])
 
     def test_rotor_iv_n(self):
         test_rotor = Rotor('IV')
-        self.assertEqual('H', test_rotor.rotor['N'])
+        self.assertEqual('H', test_rotor.rotor[13])
 
     def test_rotor_iv_z(self):
         test_rotor = Rotor('IV')
-        self.assertEqual('B', test_rotor.rotor['Z'])
+        self.assertEqual('B', test_rotor.rotor[25])
 
     def test_rotor_v_a(self):
         test_rotor = Rotor('V')
-        self.assertEqual('V', test_rotor.rotor['A'])
+        self.assertEqual('V', test_rotor.rotor[0])
 
     def test_rotor_v_b(self):
         test_rotor = Rotor('V')
-        self.assertEqual('Z', test_rotor.rotor['B'])
+        self.assertEqual('Z', test_rotor.rotor[1])
 
     def test_rotor_v_c(self):
         test_rotor = Rotor('V')
-        self.assertEqual('B', test_rotor.rotor['C'])
+        self.assertEqual('B', test_rotor.rotor[2])
 
     def test_rotor_v_d(self):
         test_rotor = Rotor('V')
-        self.assertEqual('R', test_rotor.rotor['D'])
+        self.assertEqual('R', test_rotor.rotor[3])
 
     def test_rotor_v_f(self):
         test_rotor = Rotor('V')
-        self.assertEqual('I', test_rotor.rotor['F'])
+        self.assertEqual('I', test_rotor.rotor[5])
 
     def test_rotor_v_n(self):
         test_rotor = Rotor('V')
-        self.assertEqual('H', test_rotor.rotor['N'])
+        self.assertEqual('H', test_rotor.rotor[13])
 
     def test_rotor_v_y(self):
         test_rotor = Rotor('V')
-        self.assertEqual('C', test_rotor.rotor['Y'])
+        self.assertEqual('C', test_rotor.rotor[24])
 
     def test_rotor_v_z(self):
         test_rotor = Rotor('V')
-        self.assertEqual('K', test_rotor.rotor['Z'])
+        self.assertEqual('K', test_rotor.rotor[25])
 
     def test_rotor_1_name_error(self):
         try:
