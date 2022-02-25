c4879569e293e7a8bb427848013430ff7d779484
earlution <earlution@gmail.com>
2022-02-25 13:26:03 +0000

    Updated Keyboard.press()
    - improved validation

--- source/keyboard.py
+++ source/keyboard.py
@@ -3,8 +3,10 @@ class Keyboard:
         pass
 
     def press(self, letter=None):
-        if type(letter) != str:
-            return input('Type a letter: ').upper()[0]
+        if type(letter) != str or (ord(letter.upper()) < 65 or (ord(letter.upper()) > 90)):
+            letter = input('Type a letter: ').upper()[0]
+            if type(letter) != str or (ord(letter.upper()) < 65 or (ord(letter.upper()) > 90)):
+                return ''
         else:
-            return letter
+            return letter.upper()
 
