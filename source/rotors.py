b163daf20b79e3fc727c33a98155781a3c99a6b8
earlution <earlution@gmail.com>
2022-02-27 18:28:24 +0000

    Updated Rotors
    - deleted legacy func.

--- source/rotors.py
+++ source/rotors.py
@@ -206,18 +206,6 @@ class Rotor:
         reverse_encodings = list(dict_values)
         return reverse_encodings
 
-    # @TODO do we still need this, doesn't Rotor.rotate() have this responsibility?
-    '''
-    def __advance_position(self):
-        """Increments the position value of this rotor by 1, until position is 26.  After which the value cycles back
-        round to 1.
-
-        """
-
-        self.position += 1
-        self.position = self.position % 26
-    '''
-
     def set_rotor_number(self, rotor_num):
         """Sets the - from left to right - position in the rotor sub-system this rotor will occupy.
 
