721905afbb16efee649186a979ea59c50d96f0a1
earlution <earlution@gmail.com>
2022-02-27 17:44:39 +0000

    Updated Enigma.encode()
    - deleted legacy use case logic
    - this is now tested working in __main__

--- source/enigma.py
+++ source/enigma.py
@@ -48,44 +48,6 @@ class Enigma:
         :return: The encoded letter.
         """
 
-        ''' was working-ish, trying to re-imp in __main__
-        # @TODO we need a better place for this - __main__
-        num_of_rotors = 0
-
-        self.rot_af.config_factory(RotorI)
-        r1 = self.rot_af.create_rotor()
-        self.rot_af.config_factory(RotorII)
-        r2 = self.rot_af.create_rotor()
-        self.rot_af.config_factory(RotorIII)
-        r3 = self.rot_af.create_rotor()
-
-        r1.set_ring_setting(1)
-        r2.set_ring_setting(1)
-        r3.set_ring_setting(1)
-
-        r1.set_position(Enigma.enumerate_letter('A'))
-        r2.set_position(Enigma.enumerate_letter('A'))
-        r3.set_position(Enigma.enumerate_letter('A'))
-
-        self.rotors.add_rotor_to_rotors(r1)
-        num_of_rotors += 1
-        self.rotors.add_rotor_to_rotors(r2)
-        num_of_rotors += 1
-        self.rotors.add_rotor_to_rotors(r3)
-        num_of_rotors += 1
-        self.reflector = ReflectorB()
-
-        rotations = [0 for _ in range(num_of_rotors)]
-
-        plugboard_enc = self.plugboard.encode(letter)
-        rotors_enc, rotations = self.rotors.encode(plugboard_enc, rotations)
-        reflector_enc = self.reflector.encode(rotors_enc)
-        rotors_rev_enc = self.rotors.encode(reflector_enc, rotations, True)
-        letter_enc = rotors_rev_enc
-
-        return letter_enc
-        '''
-
         plugboard_enc = self.plugboard.encode(letter)
         # @TODO rotations related code smell
         rotors_enc = self.rotors.encode(plugboard_enc)
@@ -116,7 +78,7 @@ if __name__ == "__main__":
     # configure the initial rotor positions
     r1.set_position(Enigma.enumerate_letter('A'))
     r2.set_position(Enigma.enumerate_letter('A'))
-    r3.set_position(Enigma.enumerate_letter('Z'))
+    r3.set_position(Enigma.enumerate_letter('A'))
 
     # add configured rotors to rotors sub-system
     enigma.rotors.add_rotor_to_rotors(r1)
