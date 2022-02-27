6b1d192706c71b2bdce90986cbe599bb559e09ad
earlution <earlution@gmail.com>
2022-02-27 10:48:53 +0000

    Updated tests in TestRotorListValue
    - to reflect change in structure

--- tests/test_rotors.py
+++ tests/test_rotors.py
@@ -26,83 +26,83 @@ class TestEnigma(unittest.TestCase):
 class TestRotorListValue(unittest.TestCase):
     def test_rotor_i_a(self):
         test_rotor = Rotor('I')
-        self.assertEqual('E', test_rotor.rotor[0])
+        self.assertEqual('E', test_rotor._encodings[0])
 
     def test_rotor_i_n(self):
         test_rotor = Rotor('I')
-        self.assertEqual('W', test_rotor.rotor[13])
+        self.assertEqual('W', test_rotor._encodings[13])
 
     def test_rotor_i_z(self):
         test_rotor = Rotor('I')
-        self.assertEqual('J', test_rotor.rotor[25])
+        self.assertEqual('J', test_rotor._encodings[25])
 
     def test_rotor_ii_a(self):
         test_rotor = Rotor('II')
-        self.assertEqual('A', test_rotor.rotor[0])
+        self.assertEqual('A', test_rotor._encodings[0])
 
     def test_rotor_ii_n(self):
         test_rotor = Rotor('II')
-        self.assertEqual('T', test_rotor.rotor[13])
+        self.assertEqual('T', test_rotor._encodings[13])
 
     def test_rotor_ii_z(self):
         test_rotor = Rotor('II')
-        self.assertEqual('E', test_rotor.rotor[25])
+        self.assertEqual('E', test_rotor._encodings[25])
 
     def test_rotor_iii_a(self):
         test_rotor = Rotor('III')
-        self.assertEqual('B', test_rotor.rotor[0])
+        self.assertEqual('B', test_rotor._encodings[0])
 
     def test_rotor_iii_n(self):
         test_rotor = Rotor('III')
-        self.assertEqual('N', test_rotor.rotor[13])
+        self.assertEqual('N', test_rotor._encodings[13])
 
     def test_rotor_iii_z(self):
         test_rotor = Rotor('III')
-        self.assertEqual('O', test_rotor.rotor[25])
+        self.assertEqual('O', test_rotor._encodings[25])
 
     def test_rotor_iv_a(self):
         test_rotor = Rotor('IV')
-        self.assertEqual('E', test_rotor.rotor[0])
+        self.assertEqual('E', test_rotor._encodings[0])
 
     def test_rotor_iv_n(self):
         test_rotor = Rotor('IV')
-        self.assertEqual('H', test_rotor.rotor[13])
+        self.assertEqual('H', test_rotor._encodings[13])
 
     def test_rotor_iv_z(self):
         test_rotor = Rotor('IV')
-        self.assertEqual('B', test_rotor.rotor[25])
+        self.assertEqual('B', test_rotor._encodings[25])
 
     def test_rotor_v_a(self):
         test_rotor = Rotor('V')
-        self.assertEqual('V', test_rotor.rotor[0])
+        self.assertEqual('V', test_rotor._encodings[0])
 
     def test_rotor_v_b(self):
         test_rotor = Rotor('V')
-        self.assertEqual('Z', test_rotor.rotor[1])
+        self.assertEqual('Z', test_rotor._encodings[1])
 
     def test_rotor_v_c(self):
         test_rotor = Rotor('V')
-        self.assertEqual('B', test_rotor.rotor[2])
+        self.assertEqual('B', test_rotor._encodings[2])
 
     def test_rotor_v_d(self):
         test_rotor = Rotor('V')
-        self.assertEqual('R', test_rotor.rotor[3])
+        self.assertEqual('R', test_rotor._encodings[3])
 
     def test_rotor_v_f(self):
         test_rotor = Rotor('V')
-        self.assertEqual('I', test_rotor.rotor[5])
+        self.assertEqual('I', test_rotor._encodings[5])
 
     def test_rotor_v_n(self):
         test_rotor = Rotor('V')
-        self.assertEqual('H', test_rotor.rotor[13])
+        self.assertEqual('H', test_rotor._encodings[13])
 
     def test_rotor_v_y(self):
         test_rotor = Rotor('V')
-        self.assertEqual('C', test_rotor.rotor[24])
+        self.assertEqual('C', test_rotor._encodings[24])
 
     def test_rotor_v_z(self):
         test_rotor = Rotor('V')
-        self.assertEqual('K', test_rotor.rotor[25])
+        self.assertEqual('K', test_rotor._encodings[25])
 
     def test_rotor_1_name_error(self):
         try:
@@ -381,6 +381,14 @@ class TestRotorRing(unittest.TestCase):
         self.assertEqual(2, ring_pos_result)
         self.assertEqual('M', encode_result)
 
+
+class TestRotorsAbstractFactory(unittest.TestCase):
+
+    def test_is_a_rotor(self):
+        rotors = Rotors()
+        rotor = rotors.setup(RotorII)
+        self.assertEqual(True, isinstance(rotor, Rotor))
+        print(type(rotor))
 
 if __name__ == '__main__':
     unittest.main()
